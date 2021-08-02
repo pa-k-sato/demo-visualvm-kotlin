ddf058ebb82c0a50d0a019161f5dedb3ece4a1d4
pa-k-sato <kenji.sato@Phoneappli.net>
2021-08-02 16:59:37 +0900

    add logging

--- src/main/kotlin/net/pa/demo/visualvm/some/SomeService.kt
+++ src/main/kotlin/net/pa/demo/visualvm/some/SomeService.kt
@@ -1,13 +1,23 @@
 package net.pa.demo.visualvm.some
 
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 
 private const val LOOP_COUNT = 9_999_999
 
 @Service
 class SomeService {
+    val logger: Logger = LoggerFactory.getLogger(this.javaClass)
+
     fun doWithForLoop() {
         val cls = SomeClass(LOOP_COUNT)
-        println("initialized!")
+        logger.info("initialized!")
+
+        val after = cls.list.map {
+            it.plus(cls.someMethod())
+        }
+
+        logger.info("Done! ( %,d count)".format(after.size))
     }
 }
